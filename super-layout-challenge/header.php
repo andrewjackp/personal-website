b3ece9cfad885215cd82f35c9fc8e470dbf97823
Andy <andrewjackp@gmail.com>
2024-06-13 14:45:33 -0700

    Added php, more sections to grid-responsive

---
+++ super-layout-challenge/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Super Layout Challenge">
+	<meta property='og:image' content='https://peprojects.dev/beta-one/andy/super-layout-challenge/images/meta-wrestle.jpg'>
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+
+	<title>Super Layout Challenge</title>
+</head>
+<body>
+	<header class='site-header'>
+			<div class="inner-column">
+				<picture class='poster'>
+					<img src="images/ajpw-logo.png" alt="header">
+				</picture>
+				<div class="info">
+					<span>All Japan Pro Wrestling's Four Pillars</span>
+				</div>
+			</div>
+		</header>
+	<main>
--- super-layout-challenge/index.php
+++ super-layout-challenge/index.php
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="Super Layout Challenge">
-	<meta property='og:image' content='https://peprojects.dev/beta-one/andy/super-layout-challenge/images/meta-wrestle.jpg'>
-	<link rel="stylesheet" type="text/css" href="styles/style.css">
-
-	<title>Super Layout Challenge</title>
-</head>
-<body>
-	<header class='site-header'>
-			<div class="inner-column">
-				<picture class='poster'>
-					<img src="images/ajpw-logo.png" alt="header">
-				</picture>
-				<div class="info">
-					<span>All Japan Pro Wrestling's Four Pillars</span>
-				</div>
-			</div>
-		</header>
-	<main>
+<?php include('header.php'); ?>
 		<section class="welcome">
 			<div class="inner-column">
 				<article class='card'>
@@ -149,15 +127,6 @@
 				
 		</section>
 	</main>
-	<footer class='site-footer'>
-			<div class="inner-column">
-				<ul>
-					<li>
-						<a href="#">stuff</a>
-						<a href="#">stuff</a>
-					</li>
-				</ul>
-			</div>
-		</footer>
+<?php include('footer.php'); ?>
 </body>
 </html>
