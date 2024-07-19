3da80b7bd1200fe8055978cfd51aaeb6fcdada3c
Andy <andrewjackp@gmail.com>
2024-07-19 15:23:16 -0700

    add more drum details

--- drumming/index.php
+++ drumming/index.php
@@ -9,13 +9,13 @@
 					
 						<h2 class='mid-voice'>Goon</h2>
 
-						<p class='soft-voice'>Hour of Green Evening, Paint By Numbers</p>
+						<p class='soft-voice'>Hour of Green Evening LP, Paint By Numbers EP</p>
 					</div>
 
 					<div class="info">
 						<h2 class='mid-voice'>Sheer</h2>
 
-						<p class='soft-voice'>Psychic Quarry</p>
+						<p class='soft-voice'>Psychic Quarry EP, various singles</p>
 					</div>
 
 					<div class="info">
@@ -23,13 +23,17 @@
 							<em>Live Drums</em>
 						</h1>
 						<h2 class='mid-voice'>Justus Proffit</h2>
-						<p class='soft-voice'>2021</p>
+						<p class='soft-voice'>2021 Tour</p>
+					</div>
+
+					<div class="info">
+						<h2 class='mid-voice'>Goon</h2>
+						<p class='soft-voice'>2018, 2019, 2022, 2023 Tours</p>
 					</div>
 
 					<div class="info">
 						<h2 class='mid-voice'>Draag</h2>
-
-						<p class='soft-voice'>2024</p>
+						<p class='soft-voice'>2024 Tour</p>
 					</div>
 
 
