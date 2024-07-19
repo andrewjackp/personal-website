865665ba4a04cbb0f8b24b12e97d7d4ae9f77524
Andy <andrewjackp@gmail.com>
2024-07-19 15:20:28 -0700

    add drumming page

---
+++ drumming/index.php
@@ -0,0 +1,53 @@
+<?php include('header.php'); ?>
+
+<section class='welcome'>
+				<inner-column>
+					<div class="info">
+						<h1 class='loud-voice'>
+							<em>Drums</em>
+						</h1>
+					
+						<h2 class='mid-voice'>Goon</h2>
+
+						<p class='soft-voice'>Hour of Green Evening, Paint By Numbers</p>
+					</div>
+
+					<div class="info">
+						<h2 class='mid-voice'>Sheer</h2>
+
+						<p class='soft-voice'>Psychic Quarry</p>
+					</div>
+
+					<div class="info">
+						<h1 class='loud-voice'>
+							<em>Live Drums</em>
+						</h1>
+						<h2 class='mid-voice'>Justus Proffit</h2>
+						<p class='soft-voice'>2021</p>
+					</div>
+
+					<div class="info">
+						<h2 class='mid-voice'>Draag</h2>
+
+						<p class='soft-voice'>2024</p>
+					</div>
+
+
+				</inner-column>
+			</section>
+		</main>
+	
+		<footer class='site-footer'>
+			<inner-column>
+				<a href="#">
+					<picture>
+						<img src="../andy-polito/images/cheese.svg" alt="cheese-img">
+					</picture>
+				</a>
+					<nav>
+						<a href="../index.html">Home</a>
+					</nav>
+			</inner-column>
+		</footer>
+	</body>
+</html>
