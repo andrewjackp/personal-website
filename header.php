6586d87bff108515448ca5c04d5e83226a07fbf3
Andy <andrewjackp@gmail.com>
2024-07-18 21:48:43 -0700

    add php headers

---
+++ header.php
@@ -0,0 +1,28 @@
+<html lang='en'>
+	<head>
+	<meta charset='utf-8'>
+	<meta name='viewport' content='width=device-width	initial-scale=1'>
+	<title>Andy Polito</title>
+	<meta name='description' content='Andy Polito'>
+	<meta property='og:image' content='https://peprojects.dev/beta-one/andy/layout-garden/images/meta-wrestle.jpg'>
+	<link rel="stylesheet" href="andy-polito/css/style.css">
+	</head>
+	<body>
+		<header class='site-header'>
+			<inner-column>
+				<a href="#">
+					<picture>
+						<img src="andy-polito/images/wrestling-ring.svg" alt="wrestling-ring">
+					</picture>
+				</a>
+					<nav>
+						<a href="https://peprojects.dev/beta-one/andy/layout-garden/">Layout Garden</a>	
+						
+						<a href="https://peprojects.dev/beta-one/andy/goals/index.html">Goals</a>	
+						<a href="https://peprojects.dev/beta-one/andy/resume/index.html">Resume</a>	
+
+						<a href="https://peprojects.dev/beta-one/andy/challenge-1/business-card-website/drums.html">Drumming</a>	
+					</nav>
+			</inner-column>
+		</header>
+		<main class='page-content'>
