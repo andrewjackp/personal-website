50281d588753979fd862e0b824cde21a2a1d23cd
Andy <andrewjackp@gmail.com>
2024-07-19 19:57:41 -0700

    add fixed links

--- modules/footer.php
+++ modules/footer.php
@@ -10,8 +10,8 @@
 						<a href="index.php">Home</a>
 						<a href="https://peprojects.dev/beta-one/andy/layout-garden/">Layout Garden</a>	
 						<a href="goals/index.php">Goals</a>	
-						<a href="../../resume">Resume</a>	
-						<a href="../../drumming">Drumming</a>	
+						<a href="resume/index.php">Resume</a>	
+						<a href="drumming/index.php">Drumming</a>	
 					</nav>
 			</inner-column>
 		</footer>
