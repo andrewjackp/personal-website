a4d4ea9a20fa616458085ff1136728f7cb7b2ca3
Andy <andrewjackp@gmail.com>
2024-07-20 10:21:36 -0700

    add links

--- modules/footer.php
+++ modules/footer.php
@@ -7,11 +7,11 @@
 					</picture>
 				</a>
 					<nav>
-						<a href="#">Home</a>
+						<a href="../index.php">Home</a>
 						<a href="https://peprojects.dev/beta-one/andy/layout-garden/">Layout Garden</a>	
 						<a href="../../goals">Goals</a>	
-						<a href="../resume">Resume</a>	
-						<a href="../drumming">Drumming</a>	
+						<a href="../../resume">Resume</a>	
+						<a href="../../drumming">Drumming</a>	
 					</nav>
 			</inner-column>
 		</footer>
