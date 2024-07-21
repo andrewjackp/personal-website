0ef918617b039503795f2cd8d8908d87c546f15c
Andy <andrewjackp@gmail.com>
2024-07-20 18:42:01 -0700

    add new footer pic

--- modules/footer.php
+++ modules/footer.php
@@ -1,11 +1,6 @@
 <link rel="stylesheet" href="/css/style.css">		
 		<footer class='site-footer'>
 			<inner-column>
-				<a href="#">
-					<picture>
-						<img src="images/cheese.svg" alt="cheese-img">
-					</picture>
-				</a>
 					<nav>
 						<a href="../index.php">Home</a>
 						<a href="https://peprojects.dev/beta-one/andy/layout-garden/">Layout Garden</a>	
