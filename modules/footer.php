49a3929cd19a04e9be80296c432d753c28a90017
Andy <andrewjackp@gmail.com>
2024-07-22 17:20:23 -0700

    add link
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- modules/footer.php
+++ modules/footer.php
@@ -1,9 +1,9 @@
+<?php include('config.php'); ?>
 <base href="<?=BASE_URL?>">
 <link rel="stylesheet" href="css/style.css">		
 		<footer class='site-footer'>
 				<inner-column>
 					<nav>
-						<base href="<?=BASE_URL?>">
 						<a href="../index.php">Home</a>
 						<a href="https://peprojects.dev/beta-one/andy/layout-garden/">Layout Garden</a>	
 						<a href="goals.php">Goals</a>	
