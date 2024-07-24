7f6887dd72191ed3b3167915c2ebb74b0f600dd7
Andy <andrewjackp@gmail.com>
2024-07-23 19:16:09 -0700

    add homepage in its own module
    Signed-off-by: Andy <andrewjackp@gmail.com>

---
+++ landing/landing.php
@@ -0,0 +1,19 @@
+<landing-container>
+	<div class="info">
+		<h1 class='loud-voice'>
+			Andy Polito
+		</h1>
+			<h2 class='mid-voice'>
+			Front End Web Developer, Web Designer, Drummer 
+			</h2>
+				<p class='soft-voice'>
+					Hit me up if you want to build something great! 
+					If you don't know what you want, that's okay. 
+					We can talk real life things like <a href="https://www.cagematch.net/" target="_blank">pro-wrestling</a>.
+				</p>
+	</div>
+				<div class="button-container">
+					<a href="resume.php" class="site-button">Work History</a>
+					<a href="https://codepen.io/collection/JGqLyr" target="_code-pen" class="site-button codepen">View CodePen Collection</a>
+				</div>
+</landing-container>
