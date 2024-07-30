4a60763dce6466ce7e59d301a265ec76fd2a64c0
Andy <andrewjackp@gmail.com>
2024-07-29 23:11:05 -0700

    add padding block to layout-garden sections
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- resume/index.php
+++ resume/index.php
@@ -7,36 +7,39 @@
 					</h1>
 					<h2 class='loud-voice'>Front End Web Developer and Drummer</h1>
 					<text-container>
+
+						<div class="info">
+							<h2 class='mid-voice'>Freelance</h2>
+								<p class='ink-voice'>2021, 2022, 2023 - Present</p>
+								<p class='soft-voice'>Building efficient, responsive, visually relaxing websites</p>
+								<p class="soft-voice">Tools used: HTML, CSS, JavaScript, PHP</p>
+					</div>
+
+						<div class="info">
+						<h2 class='mid-voice'>Yay For You</h2>
+						<p class='ink-voice'>June 2021 - June 2024</p>
+							<p class='soft-voice'>E-Commerce Website</p>
+							<p class="soft-voice">Worked with the team on a Shopify website.</p>
+							<p class="soft-voice">I was in charge of most of the HTML and CSS customization</p>
+							<p class="soft-voice">Tools used: HTML, CSS, JavaScript, Shopify</p>
+					</div>
+
 						<div class="info">
 							<h2 class='mid-voice'>Court Avenue</h2>
-								<p class='ink-voice'>Tools used: HTML, CSS, JavaScript, Vue, WordPress, Shopify</p>
+								<p class='ink-voice'>April 2022 - July 2022</p>
 								<p class='soft-voice'>Worked on Dell: Day in The Life</p>
 								<p class="soft-voice">A web app using Vue that used a scroll based animation to give 	the perspective of a day in the life of a Dell client.</p>
+								<p class="soft-voice">Tools used: HTML, CSS, JavaScript, WordPress, Shopify</p>
 						</div>
 
 					<div class="info">
 						<h2 class='mid-voice'>Enterprise Security Inc</h2>
-						<p class='ink-voice'>Tools used: HTML, CSS, C#, Blazor, SQL, VB6</p>
+						<p class='ink-voice'>Aug 2021 - April 2022</p>
 							<p class='soft-voice'>Employee Dashboard App. </p>
 							<p class="soft-voice">Features included: log hours, take notes, and mark jobs complete or not.</p> 
 							<p class="soft-voice">I did most of the HTML and CSS. The data layer was done in collaboration with senior software developers using C#, SQL, and Blazor for the frontend framework.</p>
+							<p class="soft-voice">Tools used: HTML, CSS, C#, Blazor, SQL, VB6</p>
 					</div>
-
-					<div class="info">
-						<h2 class='mid-voice'>Yay For You</h2>
-						<p class='ink-voice'>Shopify, HTML, CSS, JavaScript</p>
-							<p class='soft-voice'>E-Commerce Website</p>
-							<p class="soft-voice">Worked with the team on a Shopify website.</p>
-							<p class="soft-voice">I was in charge of most of the HTML and CSS customization</p>
-					</div>
-
-					<div class="info">
-						<h2 class='mid-voice'>Freelance</h2>
-						<p class='ink-voice'>HTML, CSS, JavaScript, PHP</p>
-							<p class='soft-voice'>Building efficient, responsive, visually relaxing websites</p>
-					</div>
-
-
 					<div class="info">
 						<h2 class='loud-voice'> 
 						Education
