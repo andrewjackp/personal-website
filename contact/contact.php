e8b9049aedfa934c6b5d38afd7cbf55f82f19dcc
Andy <andrewjackp@gmail.com>
2024-08-01 18:52:40 -0700

    add more contact info
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- contact/contact.php
+++ contact/contact.php
@@ -1,2 +1,15 @@
 <?php include('../header.php'); ?>
+
+<section>
+	<inner-column>
+		<h1 class="attention-voice">Contact</h1>
+		<text-container>
+			<div class="info">
+				<h2 class='mid-voice'>Email</h1>
+					<span class="ink-voice">andrewjackp@gmail.com</span>
+			</div>
+		</text-container>
+	</inner-column>
+</section>
+
 <?php include('../modules/footer.php'); ?>
