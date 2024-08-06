63b799a4529febd9854317de8e26165fba78d153
Andy <andrewjackp@gmail.com>
2024-08-05 22:16:35 -0700

    add super-ultra-responsive-layout
    Signed-off-by: Andy <andrewjackp@gmail.com>

---
+++ super-ultra-responsive-layout/index.php
@@ -0,0 +1,231 @@
+<html lang='en'>
+	<head>
+	<meta charset='utf-8'>
+	<meta name='viewport' content='width=device-width	initial-scale=1'>
+	<title>Andy Polito</title>
+	<meta name='description' content='Andy Polito'>
+	<meta property='og:image' content='[[meta-img]]'>
+	<link rel="stylesheet" href="/css/style.css">
+	</head>
+	<body>
+		<header class='layout-site-header'>
+			<inner-column>
+				<nav class="site-nav">
+					<div class="links">
+						<a href="#">LOGO</a>
+						<a href="#">Some page</a>
+						<a href="#">"Masthead"</a>
+					</div>
+					<div class="sign-in">
+						<a href="#">Sign-in</a>
+					</div>
+				</nav>
+			</inner-column>
+		</header>
+		
+		<main class='page-content'>
+
+			<section class="diptych">
+				<inner-column>
+					<graphic-diptych>
+						<picture>
+							<img src="https://peprojects.dev/images/square.jpg" alt="square-img">
+						</picture>
+						<div class="info">
+							<h1 class="loud-voice">
+								Hello! This is a 'diptych!''
+							</h1>
+							<p class="quiet-voice">You can call it whatever you would like. This is a bunch of text that go here like Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio maxime ab illo eligendi commodi.</p>
+						</div>
+					</graphic-diptych>
+				</inner-column>
+			</section>
+
+			<section class="call-to-action">
+				<inner-column>
+					<action-diptych>
+						<div class="info">
+							<h1 class="loud-voice">
+								This is a "call to action"
+							</h1>
+						<p class="quiet-voice">
+							These aren't names you should memorize. We're just making them up Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+						</p>
+					
+					</div>
+						<button class="site-button">HERE'S THE ACTION!</button>
+					</action-diptych>
+				</inner-column>
+			</section>
+
+			<section class="article-grid">
+				<inner-column>
+					<article class="article-card">
+						<action-diptych>
+							<div class="info">
+								<h1 class="loud-voice">
+									This is an "article grid" module. This is its heading. 
+								</h1>
+								<p class="quiet-voice">
+									These aren't names you should memorize. We're just making them up Lorem 
+						</p>
+									</div>
+								</action-diptych>
+							</article>
+					<ul class="article-list">
+						<li class="article-list-item">
+							<article class="article-card">
+								<action-diptych>
+									<div class="info">
+										<h1 class="loud-voice">
+											This is an "article grid" module. This is its heading. 
+										</h1>
+											<p class="quiet-voice">
+											These aren't names you
+						</p>
+											
+									</div>
+									<button class="site-button">ACT!</button>
+								</action-diptych>
+							</article>
+						</li>
+						<li class="article-list-item">
+							<article class="article-card">
+								<action-diptych>
+									<div class="info">
+										<h1 class="loud-voice">
+											This is an "article grid" module. This is its heading. 
+										</h1>
+											<p class="quiet-voice">
+											These aren't names you should memorize. We're just making them up Lorem 
+						</p>
+									</div>
+									<button class="site-button">HERE'S THE ACTION!</button>
+								</action-diptych>
+							</article>
+						</li>
+						<li class="article-list-item">
+							<article class="article-card">
+								<action-diptych>
+									<div class="info">
+										<h1 class="loud-voice">
+											This is an "article grid" module. This is its heading. 
+										</h1>
+											<p class="quiet-voice">
+											These aren't names you should memorize. We're just making them up Lorem 
+						</p>
+									</div>
+											<button class="site-button">HERE'S THE ACTION!</button>
+								</action-diptych>
+							</article>
+						</li>
+						<li class="article-list-item">
+							<article class="article-card">
+								<action-diptych>
+									<div class="info">
+										<h1 class="loud-voice">
+											This is an "article grid" module. This is its heading. 
+										</h1>
+											<p class="quiet-voice">
+											These aren't names you should memorize. We're just making them up Lorem 
+						</p>
+									</div>
+									<button class="site-button">HERE'S THE ACTION!</button>
+								</action-diptych>
+							</article>
+						</li>
+						<li class="article-list-item">
+							<article class="article-card">
+								<action-diptych>
+									<div class="info">
+										<h1 class="loud-voice">
+											This is an "article grid" module. This is its heading. 
+										</h1>
+											<p class="quiet-voice">
+											These aren't names you should memorize. We're just making them up Lorem 
+						</p>
+									</div>
+									<button class="site-button">HERE'S THE ACTION!</button>
+								</action-diptych>
+							</article>
+						</li>
+						<li class="article-list-item">
+							<article class="article-card">
+								<action-diptych>
+									<div class="info">
+										<h1 class="loud-voice">
+											This is an "article grid" module. This is its heading. 
+										</h1>
+											<p class="quiet-voice">
+											These aren't names you should memorize. We're just making them up Lorem 
+						</p>
+									</div>
+									<button class="site-button">HERE'S THE ACTION!</button>
+								</action-diptych>
+							</article>
+						</li>
+					</ul>
+				</inner-column>
+			</section>
+
+			<section class="call-to-action-ii">
+				<inner-column>
+					<action-diptych>
+						<div class="info">
+							<h1 class="loud-voice">
+								This is a "call to action"
+							</h1>
+						<p class="quiet-voice">
+							These aren't names you should memorize. We're just making them up Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+						</p>
+					
+					</div>
+						<button class="site-button">HERE'S THE ACTION!</button>
+					</action-diptych>
+				</inner-column>
+			</section>
+		</main>
+		<footer class='s-u-r-site-footer'>
+			<inner-column>
+				<section class="footer-text">
+					<action-diptych>
+						<div class="info">
+						<h1 class="loud-voice">
+							This is the site footer and it has a 'site-map' module in it
+						</h1>
+						<p class="quiet-voice">
+							Pretty standard thing right?
+						</p>
+						</div>
+					</action-diptych>
+				</section>
+				
+			<section class="site-map">
+				<site-map>
+					<nav class='site-map-nav'>
+						<div class="page-links">
+							<a href="#">LOGO</a>
+							<a href="#">Some page</a>
+							<a href="#">Some other page</a>
+						</div>
+						<div class="sign-in">
+							<a href="#">Sign-in</a>
+						</div>
+						<div class="legal-links">
+							<a href="#">Legal thing a</a>
+							<a href="#">Legal thing v</a>
+						</div>
+						<div class="social-media-links">
+							<a href="#">social thing a</a>
+							<a href="#">social thing b</a>
+							<a href="#">social thing c</a>
+							<a href="#">social thing d</a>
+						</div>
+				</nav>
+			</site-map>
+			</section>
+
+			</inner-column>
+		</footer>
+	</body>
+</html>	
