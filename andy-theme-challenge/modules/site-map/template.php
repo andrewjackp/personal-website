b507347f229ab6d61ad8f5d5534d6ec9ed119f1f
Andy <andrewjackp@gmail.com>
2024-08-21 10:59:07 -0700

    Add theme challenge
    Signed-off-by: Andy <andrewjackp@gmail.com>

---
+++ andy-theme-challenge/modules/site-map/template.php
@@ -0,0 +1,67 @@
+
+<site-map>
+	<nav class='site-menu'>
+		<ul>
+			<li>
+				<a href='#'>
+					<?php include('modules/logo.php'); ?>
+				</a>
+			</li>
+
+			<li>
+				<a href='#'>Safety</a>
+			</li>
+
+			<li>
+				<a href='#'>Our Story</a>
+			</li>
+		</ul>
+	</nav>
+
+	<nav class='x-menu'>
+		<h3 class='strong-voice'>Guest Resources</h3>
+
+		<ul>
+			<li><a href='#'>FAQs</a></li>
+			<li><a href='#'>Reservations</a></li>
+			<li><a href='#'>Group Travel</a></li>
+			<li><a href='#'>Operators Agreement</a></li>
+			<li><a href='#'>NA Conditions</a></li>
+		</ul>
+	</nav>
+
+	<nav class='x-menu'>
+		<h3 class='strong-voice'>Connect</h3>
+		
+		<ul>
+			<li><a href='#'>Contact Us</a></li>
+			<li><a href='#'>Instagram</a></li>
+			<li><a href='#'>LinkedIn</a></li>
+			<li><a href='#'>X</a></li>
+		</ul>
+	</nav>
+
+	<nav class='x-menu'>
+		<h3 class='strong-voice'>Sign Up</h3>
+		
+		<ul>
+			<li><a href='#'>Email</a></li>
+
+			<email-content>
+				<p>By subscribing you are accepting to receive marketing info etc Privacy Policy</p>
+					<a href="#"><picture><img src="images/Icon.svg" alt="email" class="email-icon"></picture></a>
+			</email-content>
+		
+		</ul>
+	</nav>
+</site-map>
+
+<site-disclaimer>
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat  
+	</p>
+	<ul>
+		<li><a href="#">Privacy Policy</a></li>
+		<li><a href="#">Terms of Use</a></li>
+	</ul>
+</site-disclaimer>
