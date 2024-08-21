0e40fd9a5a4568cd0abdf097925b2998bd4559ce
Andy <andrewjackp@gmail.com>
2024-08-21 16:15:51 -0700

    Add meta img
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- wrestle-docs/index.php
+++ wrestle-docs/index.php
@@ -15,6 +15,7 @@
 	<head>
 		<meta charset='utf-8'>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta property="og:image" content="images/still-real.jpg">
 		<title><?=$page?></title>
 		<!-- meta -->
 		<link rel="stylesheet" href="css/site.css">
