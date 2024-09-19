0dfd530dcb803528efd45fa0c49d411f08284fef
Andy <andrewjackp@gmail.com>
2024-09-18 22:29:07 -0700

    Add json to resume
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- resume/index.php
+++ resume/index.php
@@ -1,40 +1,11 @@
 
 <?php
-	$resume = [
-    "positions" => [
-        [
-            "type" => "Freelance",
-            "years" => "2021 to present",
-            "tasks" => "Building efficient, responsive, visually relaxing websites",
-            "tools" => "HTML, CSS, JavaScript, PHP"
-        ],
-        [
-            "type" => "Yay For You",
-            "years" => "2021 to 2024",
-            "tasks" => "Worked with the team on a Shopify website.",
-            "tools" => "HTML, CSS, JavaScript, Shopify"
-        ],
-        [
-            "type" => "Court Avenue",
-            "years" => "2022",
-            "tasks" => "Worked on Dell: Day in The Life. A web app using Vue that used a scroll-based animation to give the perspective of a day in the life of a Dell client.",
-            "tools" => "HTML, CSS, JavaScript, WordPress, Shopify"
-        ],
-    ],
-   "education" => [
-   	[
-   		"institution" => "Pasadena City College",
-         "studies" => "Communications"
-   	],
-   	[
-   		"institution" => "Perpetual Education",
-         "studies" => "Design For The Web"
-   	]
-   ] 
-];
+	$resume_data = getData('data/resume.json');
+    $positions = $resume_data["positions"];
+    $schools = $resume_data["education"];
 ?>
 
-<?php foreach( $resume["positions"] as $position ) { ?>
+<?php foreach( $positions as $position ) { ?>
 
 	<div class='info'>
 		<h2 class='mid-voice'><?=$position["type"]?></h2>
@@ -45,7 +16,7 @@
 
 <?php } ?>
 
-<?php foreach( $resume["education"] as $schooling ) { ?>
+<?php foreach( $schools as $schooling ) { ?>
 
 	<div class='info'>
 		<h2 class='mid-voice'><?=$schooling["institution"]?></h2>
