8b42a43fde51e419d5ce0037f067e22f2cd2cbeb
Andy <andrewjackp@gmail.com>
2024-10-09 08:42:36 -0700

    Add local storage
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- javascript/todo-app/script.js
+++ javascript/todo-app/script.js
@@ -14,63 +14,87 @@ function ToDo(todos = [], idMarker = 0) {
 	}
 }
 
-const task1 = new ToDo("Dig hole", 7);
-const task2 = new ToDo("fly to mars", 1);
+let myTasks = [
+	new ToDo ("make coffee", 1),
+	new ToDo ("walk dog", 2),
+	new ToDo ("make lunch", 3)
+];
 
-task1.complete();
+localStorage.myTasks = JSON.stringify(myTasks);
 
-console.log(task2.idMarker);
+let data = JSON.parse(localStorage.getItem('myTasks'));
 
-task2.print("add laundry");
+data.forEach(tasks => {
+	console.log(tasks)
+});
 
-ToDo.prototype.add = function(content) {
-	const todo = {
-		id: `a${this.idMarker++}`,
-		content: content
-	};
+// task1.complete();
 
-	this.todos = [...this.todos, todo];
-	this.print(`added ${content}, ${this.idMarker}`);
-}
+// console.log(task2.idMarker);
 
-task2.add("make coffee");
+// task2.print("add laundry");
 
+// ToDo.prototype.add = function(content) {
+// 	const todo = {
+// 		id: `a${this.idMarker++}`,
+// 		content: content
+// 	};
 
-const todoApp = {
+// 	this.todos = [...this.todos, todo];
+// 	this.print(`added ${content}, ${this.idMarker}`);
+// }
 
-	todos: [],
-	idMarker: 0,
+// task2.add("make coffee");
 
-	print: function(note = "") {
-		console.log(`---- ${note}`);
-		console.log('todos: ', this.todos);
-	},
+// const data = localStorage;
 
-	add: function(content) {
-	const todo = {
-		id: `a${this.idMarker++}`,
-		content: content
-	};
+// var idMarker = 0;
 
-	this.todos = [...this.todos, todo];
-	this.print(`added ${content}`);
-	},	
+// idMarker++;
 
- 	remove: function(id) {
-		this.todos.splice(id, 1);
-		this.print(`removed ${this.todos[id].content}`);
-	},
+// function initialize() {
+// 	if ( data.getItem('todos') ) {
+// 		console.log('todos');
+// 	} else {
+// 		data.setItem('todos', JSON.stringify({ idMarker: 0 }) );
+// 	};
+// };
 
- 	complete: function(id) {
-		this.todos[id].complete = true;
-		this.print(`completed: ${this.todos[id].content}`);
-	},
+// const todoApp = {
 
- 	update: function(id, content) {
-		this.todos[id].content = content;
-		this.print(`updated: ${this.todos[id].content}`);
-	},
-};
+// 	todos: [],
+// 	idMarker: 0,
+
+// 	print: function(note = "") {
+// 		console.log(`---- ${note}`);
+// 		console.log('todos: ', this.todos);
+// 	},
+
+// 	add: function(content) {
+// 	const todo = {
+// 		id: `a${this.idMarker++}`,
+// 		content: content
+// 	};
+
+// 	this.todos = [...this.todos, todo];
+// 	this.print(`added ${content}`);
+// 	},	
+
+//  	remove: function(id) {
+// 		this.todos.splice(id, 1);
+// 		this.print(`removed ${this.todos[id].content}`);
+// 	},
+
+//  	complete: function(id) {
+// 		this.todos[id].complete = true;
+// 		this.print(`completed: ${this.todos[id].content}`);
+// 	},
+
+//  	update: function(id, content) {
+// 		this.todos[id].content = content;
+// 		this.print(`updated: ${this.todos[id].content}`);
+// 	},
+// };
 
 // const otherTodo = todoApp;
 
