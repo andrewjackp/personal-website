e760907e2eeaa4bfc153d147246a3299a5337a24
Andy <andrewjackp@gmail.com>
2024-11-06 22:49:46 -0800

    Add meta tag
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- mockup-to-code/index.php
+++ mockup-to-code/index.php
@@ -2,6 +2,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name='description' content='Mockup To Code'>
 	<title>Mockup To Code</title>
 	<link rel="stylesheet" href="styles/site.css">
 </head>
