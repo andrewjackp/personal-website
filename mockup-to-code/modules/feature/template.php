508a5560895190f0c72e67b4002d864c258a787b
Andy <andrewjackp@gmail.com>
2024-11-06 22:09:11 -0800

    Add media queries to feature list
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- mockup-to-code/modules/feature/template.php
+++ mockup-to-code/modules/feature/template.php
@@ -1,11 +1,2 @@
 <ul class="feature-card-list" id="feature-card-list">
-	<li class='card-item'>
-		<article class="article-card">
-		<span class="id">id</span>
-		<text-content>
-			<h2 class="loud-voice">title</h2>
-			<p class="quiet-voice">desc</p>
-		</text-content>
-	</article>
-	</li>
 </ul>	
