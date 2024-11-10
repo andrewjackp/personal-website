c43df665bf838e9f74febac96e9e3484d8700d75
Andy <andrewjackp@gmail.com>
2024-11-10 09:56:44 -0800

    Add template 2
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- mockup-to-code/template-2/modules/product-overview/template.php
+++ mockup-to-code/template-2/modules/product-overview/template.php
@@ -1,19 +1,26 @@
 <product-overview>
+	<picture>
+		<img src="images/cloud_avatar.svg" alt="avatar">
+	</picture>
 	<span class="quiet-voice">
 		Your Visual Language Partner
 	</span>
 
 	<h2 class="loud-voice">
-		Etiam nulla lfulputate.
+		Consistency Across All Faces (and Tentacles)
 	</h2>
 
 	<span class="quiet-voice">
 		We specialize in monster-friendly design systems that scale from wiggly toes to toothy grins.
 	</span>
 
-	<video-box>
-		<picture>
-			<img src="images/play-button.svg" alt="">
+	<learn-more>
+		<a class="link-voice" href='#'>Learn More</a>
+		<a href="#">
+			<picture>
+			<img src="images/right_arrow.svg" alt="right-arrow">
 		</picture>
-	</video-box>
+		</a>
+	</learn-more>
+	
 </product-overview>
