2d8225e0b6a339852889a113fcfbbaee64676e22
Andy <andrewjackp@gmail.com>
2024-11-10 10:33:56 -0800

    Add client styles
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- mockup-to-code/template-2/modules/clients/controller.js
+++ mockup-to-code/template-2/modules/clients/controller.js
@@ -2,42 +2,26 @@ const clientQuotes = [
 	{	rating: "images/star.svg",
 		quote: "Orci vel eget in eu. Integer amet porttitor hendrerit etiam arcu, aliquet duis pretium consequat. Semper sed viverra enim ut nunc.",
 		img: "images/avatar.svg",
-		name: "Courtney Henry"
-	},
-	{	rating: "images/star.svg",
-		quote: "Tincidunt risus, blandit proin semper. Tellus ac pellentesque convallis vitae. Lorem enim cursus et consequat viverra id justo ullamcorper. Vel.",
-		img: "images/avatar.svg",
-		name: "Courtney Henry"
+		name: "Courtney Henry",
+		job: "Marketing Coordinator"
 	}
+	// {	rating: "images/star.svg",
+	// 	quote: "Tincidunt risus, blandit proin semper. Tellus ac pellentesque convallis vitae. Lorem enim cursus et consequat viverra id justo ullamcorper. Vel.",
+	// 	img: "images/avatar.svg",
+	// 	name: "Courtney Henry"
+	// }
 ];
 
 const renderClient = (item) => {
 	return `
 		<li class='client-card-item'>
-			<article class='client-article-card'>
-				<div class='star-ratings'>
-					<picture>
-					<img src="images/star.svg" alt="client-pic">
-					</picture>
-					<picture>
-					<img src="images/star.svg" alt="client-pic">
-					</picture>
-					<picture>
-					<img src="images/star.svg" alt="client-pic">
-					</picture>
-					<picture>
-					<img src="images/star.svg" alt="client-pic">
-					</picture>
-					<picture>
-					<img src="images/star.svg" alt="client-pic">
-					</picture>
-				</div>
-				
-				<p class='soft-voice'>${item.quote}</p>
+			<article class='client-article-card'>				
+				<p class='loud-voice'>${item.quote}</p>
 				<picture>
 					<img src="${item.img}" alt="client-img">
 				</picture>
-				<span class='quiet-voice'>${item.name}</span>
+				<span class='strong-voice'>${item.name}</span>
+				<span class='quiet-voice'>${item.job}</span>
 			</article>
 		</li>
 	`
