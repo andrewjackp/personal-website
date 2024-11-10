9439e033a76f24c6706d3395be56ef3ac9425d0d
Andy <andrewjackp@gmail.com>
2024-11-10 10:12:45 -0800

    Add client grid
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- mockup-to-code/template-2/modules/feature/controller.js
+++ mockup-to-code/template-2/modules/feature/controller.js
@@ -1,33 +1,38 @@
 const featureList = [
 	{	title: "Etiam nulla lectus molestie at vulputate.",
 		description: "Neque, pulvinar vestibulum non aliquam.",
-		img: "",
-		id: 0
+		img: "images/cloud_icon.svg",
+		id: 1
 	},
 	{
 		title: "Sit quam gravida",
 		description: "Et hendrerit turpis habitant cursus semper ultrices in nunc. Erat eget etiam senectus vestibulum risus.",
-		id: 1
+		img: "images/cloud_icon.svg",
+		id: 2
 	},
 	{
 		title: "Elementum enim tempus",
 		description: "At tristique elementum metus blandit id sed vel eu a. Diam sed feugiat diam nisi est. Dictumst elit.",
-		id: 2
+		img: "images/cloud_icon.svg",
+		id: 3
 	},
 	{
 		title: "Tempor egestas augue",
 		description: "Eu metus, at orci dui sapien. Semper fames est ut gravida. Neque pellentesque risus ut odio.",
-		id: 3
+		img: "images/cloud_icon.svg",
+		id: 4
 	},
 	{
 		title: "Nulla ac lacinia",
 		description: "Molestie faucibus amet sed ut gravida iaculis sed venenatis. Pulvinar id lacus condimentum ut. Non.",
-		id: 4
+		img: "images/cloud_icon.svg",
+		id: 5
 	},
 	{
 		title: "Velit nibh a",
 		description: "Ac morbi integer orci justo lobortis elementum, massa pulvinar quisque. Porttitor sit convallis quis.",
-		id: 5
+		img: "images/cloud_icon.svg",
+		id: 6
 	}
 ];
 
@@ -35,7 +40,9 @@ const renderFeature = (feature) => {
 	return `
 		<li class='card-item'>
 			<article class="article-card">
-				<span class="id-voice">${feature.id}</span>
+				<picture>
+					<img src="${feature.img}" alt="cloud-pic">
+				</picture>
 				<text-content>
 					<h2 class="soft-voice">${feature.title}</h2>
 					<p class="quiet-voice">${feature.description}</p>
