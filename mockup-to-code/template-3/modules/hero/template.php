f33e26f90cf216d5a044de42665943acdaa5af27
Andy <andrewjackp@gmail.com>
2024-11-13 20:37:19 -0800

    Add gap to site-disclaimer
    Signed-off-by: Andy <andrewjackp@gmail.com>

---
+++ mockup-to-code/template-3/modules/hero/template.php
@@ -0,0 +1,15 @@
+<hero>
+	<hero-text-container>
+		<h1 class="attention-voice">
+			Monstrously Cohesive Visual Systems
+		</h1>
+		<p class="quiet-voice">
+			Creating visual languages for brands with bold identities and even bolder appetites, keeping every detail on-brand.
+		</p>
+	</hero-text-container>
+
+	<div class="button-container">
+		<a href="#" class="button">Get Started</a>
+		<a href="#" class="button free-trial">Start Free Trial</a>
+	</div>
+</hero>
