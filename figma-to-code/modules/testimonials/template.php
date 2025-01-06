0416c199e9cc9fb4f0d21708aad97171b3083940
Andy <andrewjackp@gmail.com>
2025-01-05 22:57:12 -0800

    Add customer review module
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- figma-to-code/modules/testimonials/template.php
+++ figma-to-code/modules/testimonials/template.php
@@ -1,7 +1,3 @@
-<video-page>
-	<span class='quiet-voice'>Your visual language par</span>
-	<h1 class='loud-voice'>Etiam nulla lfulputate.</h1>
-	<p class="quiet-voice">We specialize in monster-friendly design systems that scale from wiggly toes to toothy grins.
-	</p>
-	<video src=""></video>
-</video-page>
+<?php 
+	include('modules/page-header/template.php');
+?>
