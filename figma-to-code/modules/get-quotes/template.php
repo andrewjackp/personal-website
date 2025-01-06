e58a12c9e388570b3dabd0c0bc914eed63f1032b
Andy <andrewjackp@gmail.com>
2025-01-05 23:39:59 -0800

    Add get quotes template
    Signed-off-by: Andy <andrewjackp@gmail.com>

---
+++ figma-to-code/modules/get-quotes/template.php
@@ -0,0 +1,9 @@
+<quotes>
+	<p class="quiet-voice">Rhoncus, accumsan dictum</p>
+	<h1 class="attention-voice">Etiam nulla lectus amet nunc molestie at vulputate.</h1>
+	<div class="button-container">
+		<?php 
+			$content = "Get Free Quotes >";
+			include('components/blue-button/template.php');?>
+	</div>
+</quotes>
