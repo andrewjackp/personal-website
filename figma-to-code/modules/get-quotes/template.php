6afed53ebc742cc6e78da4c10c63d91d4e0c9cc0
Andy <andrewjackp@gmail.com>
2025-01-06 22:23:28 -0800

    Add new main-with-features list
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- figma-to-code/modules/get-quotes/template.php
+++ figma-to-code/modules/get-quotes/template.php
@@ -3,7 +3,7 @@
 	<h1 class="attention-voice">Etiam nulla lectus amet nunc molestie at vulputate.</h1>
 	<div class="button-container">
 		<?php 
-			$content = "Get Free Quotes >";
+			$content = "Get Free Quotes  >";
 			include('components/blue-button/template.php');?>
 	</div>
 </quotes>
