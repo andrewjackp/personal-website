8f5b08543a5e529e80602d526ed1c18e582ef5d6
Andy <andrewjackp@gmail.com>
2025-01-07 08:45:30 -0800

    Add FAQs with PHP list
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- figma-to-code/modules/faqs/template.php
+++ figma-to-code/modules/faqs/template.php
@@ -1,1 +1,21 @@
-<h1>FAQs</h1>
+<?php
+	$faqs = $props['faqs'] ?? [];
+	$props['heading'] = "Frequently Asked Questions";
+	$props['caption'] = "Pellentesque cras adipiscing tempus libero vel nullam mauris tellus. Aliquam ultrices tellus consequat amet, lectus aliquam est in neque.";
+	include('modules/page-header/template.php');
+?>
+
+<faqs>
+	<ul id='faq-list'>
+	<?php foreach($faqs as $faq) { ?>
+		<li>
+			<article class='faq-card'>
+		<details>
+			<summary><?=$faq['question']?></summary>
+			<p class="quiet-voice"><?=$faq['answer']?></p>
+		</details>
+		</article>
+		</li>	
+	<?php } ?>
+	</ul>
+</faqs>
