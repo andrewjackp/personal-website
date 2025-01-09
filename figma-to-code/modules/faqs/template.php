641ef35b115d4a46f0cd0237fed3f8c9770c1ccc
Andy <andrewjackp@gmail.com>
2025-01-08 22:00:57 -0800

    Add icons for FAQs
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- figma-to-code/modules/faqs/template.php
+++ figma-to-code/modules/faqs/template.php
@@ -2,19 +2,31 @@
 	$faqs = $props['faqs'] ?? [];
 	$props['heading'] = "Frequently Asked Questions";
 	$props['caption'] = "Pellentesque cras adipiscing tempus libero vel nullam mauris tellus. Aliquam ultrices tellus consequat amet, lectus aliquam est in neque.";
-	include('modules/page-header/template.php');
+
+	$questionStyle = $props['questionStyle'] ?? "default";
+
+	$summaryStyle = $props['summaryStyle'] ?? "default";
+
+	$faqPageStyle = $props['faqPageStyle'] ?? "default";
 ?>
 
-<faqs>
+<faqs class="faq-page-item' <?=$faqPageStyle?>">
+	<?php 
+		include('modules/page-header/template.php');
+	?>
 	<ul id='faq-list'>
 	<?php foreach($faqs as $faq) { ?>
 		<li>
 			<article class='faq-card'>
-		<details>
-			<summary><?=$faq['question']?></summary>
-			<p class="quiet-voice"><?=$faq['answer']?></p>
-		</details>
-		</article>
+
+				<details class="question-tab <?=$questionStyle?>">
+
+				<summary class="<?=$summaryStyle?>"><?=$faq['question']?></summary>
+				
+				<p class="quiet-voice"><?=$faq['answer']?></p>
+
+				</details>
+			</article>
 		</li>	
 	<?php } ?>
 	</ul>
