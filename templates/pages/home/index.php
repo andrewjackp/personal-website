93f1a454a1fefb13ec37cfde1329ab6d67f4a15d
Andy <andrewjackp@gmail.com>
2025-03-06 20:54:43 -0800

    Add flex shrink to mobile toggle
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- templates/pages/home/index.php
+++ templates/pages/home/index.php
@@ -9,3 +9,18 @@
 ?>
 	</inner-column>
 </section>
+
+<section>
+	<inner-column>
+		<?php $pageTitle = "My list of work" ?>
+		<?php include('templates/modules/page-header/template.php');?>
+	</inner-column>
+</section>
+
+<section>
+	<inner-column>
+		<?php
+			include('templates/modules/resume-list/template.php');
+		?>
+	</inner-column>
+</section>
