cf4c1dcc2cc2da14351d8acd804922c93279b526
Andy <andrewjackp@gmail.com>
2025-04-29 08:40:50 -0700

    Add spelling fix
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- templates/pages/layout-garden/concierge-cards/template.php
+++ templates/pages/layout-garden/concierge-cards/template.php
@@ -1,13 +1,13 @@
 <div class="heading-box">
-	<h1>Sabre Concierge Card Grid</h1>
-	<p>A card list inspired by <a href="https://sabrageconcierge.com/" target="_layout-garden">Sabre Concierge's</a> website.</p>
+	<h1>Sabrage Concierge Card Grid</h1>
+	<p>A card list inspired by <a href="https://sabrageconcierge.com/" target="_layout-garden">Sabre Concierge's</a> website, built by <a href="https://www.nonverbaloriginals.com/">Non Verbal Original's Agency</a>.</p>
 </div>
 
 <ul class="service-card-list">
 	<li>
 		<article class='service-card'>
 			<picture>
-				<img src="https://peprojects.dev/images/portrait.jpg" alt="pic">
+				<img src="https://peprojects.dev/images/landscape.jpg" alt="pic">
 			</picture>
 			
 			<div class="concierge-info">
@@ -52,4 +52,52 @@
 			</div>
 		</article>
 	</li>
+<!-- 	<li>
+		<article class='service-card'>
+			<picture>
+				<img src="https://peprojects.dev/images/portrait.jpg" alt="pic">
+			</picture>
+			
+			<div class="concierge-info">
+				<h2>Heading</h2>
+				<p>1</p>
+			</div>
+			
+			<div class="caption">
+				<span>creating moments</span>
+			</div>
+		</article>
+	</li>
+	<li>
+		<article class='service-card'>
+			<picture>
+				<img class='portrait' src="https://peprojects.dev/images/portrait.jpg" alt="pic">
+			</picture>
+			
+			<div class="concierge-info">
+				<h2>Heading</h2>
+				<p>1</p>
+			</div>
+				<div class="caption">
+					<span>creating moments</span>
+				</div>
+		</article>
+	</li>
+
+	<li>
+		<article class='service-card'>
+			<picture>
+				<img class='portrait' src="https://peprojects.dev/images/landscape.jpg" alt="pic">
+			</picture>
+
+			<div class="concierge-info">
+				<h2>Heading</h2>
+				<p>1</p>
+			</div>
+			
+			<div class="caption">
+				<span>creating moments</span>
+			</div>
+		</article>
+	</li> -->
 </ul>
