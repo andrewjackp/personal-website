c54f8b22f38378aa063d1da0c53499b371f05bde
Andy <andrewjackp@gmail.com>
2025-05-28 20:56:05 -0700

    Add homepage cards
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- templates/modules/home-cards/template.php
+++ templates/modules/home-cards/template.php
@@ -4,9 +4,11 @@
 			<picture>
 				<img src="https://peprojects.dev/images/landscape.jpg" alt="">
 			</picture>
+
 			<div class="text-info">
 				<span>Text here</span>
 			</div>
+
 		</article>
 	</li>
 	<li class='home-card-item'>
@@ -14,6 +16,11 @@
 			<picture>
 				<img src="https://peprojects.dev/images/landscape.jpg" alt="">
 			</picture>
+
+			<div class="text-info">
+				<span>Experience</span>
+			</div>
+
 		</article>
 	</li>
 	<li class='home-card-item'>
@@ -21,6 +28,11 @@
 			<picture>
 				<img src="https://peprojects.dev/images/landscape.jpg" alt="">
 			</picture>
+
+			<div class="text-info">
+				<span>Layout Garden</span>
+			</div>
+
 		</article>
 	</li>
 	<li class='home-card-item'>
@@ -28,13 +40,11 @@
 			<picture>
 				<img src="https://peprojects.dev/images/landscape.jpg" alt="">
 			</picture>
-		</article>
-	</li>
-	<li class='home-card-item'>
-		<article class='home-card-details'>
-			<picture>
-				<img src="https://peprojects.dev/images/landscape.jpg" alt="">
-			</picture>
+
+			<div class="text-info">
+				<span>Drums?</span>
+			</div>
+			
 		</article>
 	</li>
 </ul>
