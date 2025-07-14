ef72a9aefba99f1ce9332ad95b1e3679c329f405
Andy <andrewjackp@gmail.com>
2025-07-13 17:11:15 -0700

    add padding to buttons, call-to-action
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- templates/pages/about/index.php
+++ templates/pages/about/index.php
@@ -2,7 +2,17 @@
 	<inner-column>
 		<?php
 			$pageTitle = "About!";
+			$intro = "";
 			include('templates/modules/page-header/template.php');
 		?>
+
+		<div class="info">
+			<p>
+			I am a web developer who loves to write HTML, CSS, JavaScript, PHP, and whatever framework suits the project.
+		</p>
+
+		<p>I am also a decent drummer and happy to play on your sessions.</p>
+		</div>
+
 	</inner-column>
 </section>
