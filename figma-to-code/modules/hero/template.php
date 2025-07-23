eb4db5f6700e707b229bde4bfcb2ef449ee7d586
Andy <andrewjackp@gmail.com>
2025-07-23 06:25:29 -0700

    Add more line heights, margins for page-headers
    Signed-off-by: Andy <andrewjackp@gmail.com>

---
+++ figma-to-code/modules/hero/template.php
@@ -0,0 +1,12 @@
+<hero>
+
+	<span class="quiet-voice">
+		Your Visual Language Partner
+	</span>
+
+	<?php include('modules/page-header/template.php'); ?>
+
+	<picture>
+		<img src="https://peprojects.dev/images/landscape.jpg" alt="pic">
+	</picture>
+</hero>
