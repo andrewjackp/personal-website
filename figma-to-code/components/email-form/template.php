a48ee6b7afe475ba38861bcc14401d84210d6129
Andy <andrewjackp@gmail.com>
2025-07-29 06:31:54 -0700

    Add FAQs for temp 2
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- figma-to-code/components/email-form/template.php
+++ figma-to-code/components/email-form/template.php
@@ -2,12 +2,15 @@
 	<email-input>
 		<div class='icon'>⬤</div>
 		
-		<!-- we'd probably opt for a proper label -->
-		<input type='email' placeholder='Email Address' required />
+		<input id="email-input" type='email' placeholder='Email Address' required />
+			<button type='button'> 
+
+			<span class='arrow'>
+				<picture><img src="images/white-right-arrow.svg" alt=""></picture>
+			</span>
+
+			<span class='text'>SignUp</span>
+			</button>
+			
 	</email-input>
-	
-	<button type='button'> 
-		<span class='arrow'><picture><img src="images/white-right-arrow.svg" alt=""></picture></span>
-		<span class='text'>SignUp</span>
-	</button>
 </form>
