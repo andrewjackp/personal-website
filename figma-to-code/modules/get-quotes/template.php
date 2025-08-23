0348c353793c056de610ff3cd805c9f5a8510168
Andy <andrewjackp@gmail.com>
2025-08-23 13:32:48 -0700

    Add margin-top to page-header
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- figma-to-code/modules/get-quotes/template.php
+++ figma-to-code/modules/get-quotes/template.php
@@ -1,10 +1,12 @@
 <?php 
 	$actions = $props['actions'] ?? [];
+	$heading = $props['heading'] ?? "Unlock the value of a tailored visual system designed to evolve with your organization.";
+	$content = $props['content'] ?? "Partner With Us Today";
 ?>
 
 <quotes>
-	<p class="quiet-voice">Unlock the value of a tailored visual system designed to evolve with your organization.</p>
-	<h1 class="attention-voice">Partner With Us Today</h1>
+	<p class="quiet-voice"><?=$heading?></p>
+	<h1 class="attention-voice"><?=$content?></h1>
 	<div class="button-container">
 		<?php foreach($actions as $action):?>
 			<?php include("components/$action[buttonType]/template.php");?>
