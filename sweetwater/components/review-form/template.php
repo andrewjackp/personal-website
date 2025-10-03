6bb17234f5dc2c77ec5fe22f4991497c07c03285
Andy <andrewjackp@gmail.com>
2025-10-03 12:37:41 -0400

    Add form styling
    Signed-off-by: Andy <andrewjackp@gmail.com>

--- sweetwater/components/review-form/template.php
+++ sweetwater/components/review-form/template.php
@@ -1,3 +1,10 @@
+<?php
+	$imageList = [
+		"image one", "image two", "image three"
+	];
+
+?>
+
 <form class="review-form">
 	<h2 class="loud-voice">Submit Your Review</h2>
 	
@@ -8,15 +15,17 @@
 	<textarea type="text" id="review" name="review" rows='13'></textarea>
 	
 	<label for="add_images">Add Image (Optional)</label>
-	<div class="image-area">
-		<select name="add_images" id="add_images">
-			<option value="">No Files Selected</option>
- 			<option value="">Select Files</option>
-			<option value="">Image 1</option>
-  			<option value="">Image 2</option>
-  			<option value="">Image 3</option>
-		</select>
-	</div>
+	
+  <div class="image-area">
+    <select name="add_images" id="add_images" class='img-add'>
+      <option value="">Add Images</option>
+      <?php foreach ($imageList as $img): ?>
+        <option value="<?= $img ?>">
+          <?= $img ?>
+        </option>
+      <?php endforeach; ?>
+    </select>
+  </div>
 
   	<div><?php include('components/submit-btn/template.php'); ?></div>
 
